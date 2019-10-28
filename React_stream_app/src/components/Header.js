fad80dcd17f0c7cf05cc94a5da434740ff48978e
dmytryy <dmytrash.yura@gmail.com>
2019-10-27 17:49:50 -0700

    responsive logo

--- React_stream_app/src/components/Header.js
+++ React_stream_app/src/components/Header.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 // import { BrowserRouter, Route, Link } from 'react-router-dom';
 import GoogleAuth from './GoogleAuth';
+import logo from '../../src/images/logo.png';
 
 const Header = () => {
   return (
     <div className="ui secondary pointing menu">
-      <Link to="/" className="item">
+      <Link to="/" className="item" >
+        <img className="img-responsive" src={logo} alt="logo" />
         Streamer
       </Link>
       <div className="right menu">
